207021179191c5a0a53bc23b8e1639ba7036adce
mcyee <mingcee@skysketches.com>
2015-09-03 01:57:02 -0400

    finished implementing shunting yard algo (infix to rpn method) and deleted todo file

--- TODO.md
+++
@@ -1,3 +0,0 @@
-# TODO
-
-* implement shunting yard algorithm to process input using reverse polish notation
--- src/InfixExpr.java
+++ src/InfixExpr.java
@@ -3,11 +3,14 @@
  * Author: Ming-Cee Yee
  * Created: Sep 1, 2015
  * Description: Representation of an expression in infix notation
- * Reference: http://eddmann.com/posts/shunting-yard-implementation-in-java/
+ * References: https://en.wikipedia.org/wiki/Shunting-yard_algorithm
+ *             http://rosettacode.org/wiki/Parsing/Shunting-yard_algorithm#Java
+ *             http://eddmann.com/posts/shunting-yard-implementation-in-java/
  */
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class InfixExpr {
@@ -18,6 +21,9 @@ public class InfixExpr {
 		expr = e;
 	}
 	
+	/**
+	 * Valid operators and their precedences
+	 */
 	private enum Operator {
 		ADD      (1),
 		SUBTRACT (2),
@@ -31,88 +37,99 @@ public class InfixExpr {
 		}
 	}
 	
-	//TODO follow reference
+	/**
+	 * Valid functions
+	 */
+	private enum Function {
+		NEGATION,
+		ABSOLUTEVALUE;
+	}
 	
 	/**
-	 * isInteger() returns true if str can be converted into an integer
-	 *     (does not account for overflow)
-	 * Reference: http://stackoverflow.com/questions/237159/whats-the-best-way-to-check-to-see-if-a-string-represents-an-integer-in-java
+	 * Map of symbols to their respective operators
 	 */
-	private static boolean isInteger(String str) {
-		if (str == null) return false;
-		int length = str.length();
-		if (length == 0) return false;
-		
-		int i = 0;
-		if (str.charAt(0) == '-') {
-			if (length == 1) return false;
-			i = 1;
-		}
-		for(; i < length; i++) {
-			char c = str.charAt(i);
-			if (c < '0' || '9' < c) return false;
-		}
-		return true;
+	private static Map<String, Operator> ops = new HashMap<String, Operator>(){
+		private static final long serialVersionUID = -4931774982280911450L;
+	{
+		put("+", Operator.ADD);
+		put("-", Operator.SUBTRACT);
+		put("*", Operator.MULTIPLY);
+		put("/", Operator.DIVIDE);
+	}};
+	
+	/**
+	 * Map of symbols to their respective functions
+	 */
+	private static Map<String, Function> functions = new HashMap<String, Function>() {
+		private static final long serialVersionUID = -6495730501768339133L;
+	{
+		put("NEG", Function.NEGATION);
+		put("ABS", Function.ABSOLUTEVALUE);
+	}};
+	
+	/**
+	 * isHigherPrecedence() returns true if op has a higher precedence than sub
+	 */
+	private static boolean isHigherPrecedence(String o1, String o2) {
+		return (ops.containsKey(o2) && ops.get(o2).precedence >= ops.get(o1).precedence);
 	}
 	
 	/**
 	 * getRPN() converts the InfixExpr into Reverse Polish Notation
+	 * Follows implementation references as listed in class header (above)
 	 */
 	public String getRPN() {
-		String rpnExpr = "";
-		
-		ArrayList<String> operators = new ArrayList<>();
-		operators.add("+");
-		operators.add("-");
-		operators.add("*");
-		operators.add("/");
-		operators.add("NEG");
-		operators.add("ABS");
-		
-		ArrayDeque<String> outputQueue = new ArrayDeque<String>();
-		ArrayDeque<String> operatorStack = new ArrayDeque<String>();
+		StringBuilder output = new StringBuilder();
+		Stack<String> stack = new Stack<String>();
 		StringTokenizer st = new StringTokenizer(expr);
 		
 		while (st.hasMoreTokens()) {
 			String s = st.nextToken();
-			
-			// push integer on the output queue
-			if (isInteger(s)) {
-				outputQueue.add(s);
-			}
-			// push operator on the operator stack
-			else if (operators.contains(s)) {
-				operatorStack.push(s);
-			}
-			//TODO fix
-			// handle separators (parentheses)
-			else {
-				// pops operators off the stack onto the queue until
-				// a "(" is reached
-				while (operatorStack.peekLast() != "(") {
-					outputQueue.add(operatorStack.removeLast());
+
+			// push operator onto the stack if it has higher precedence
+			// assumes there are no right-associative operators
+			if (ops.containsKey(s)) {
+				while (!stack.isEmpty() && isHigherPrecedence(s, stack.peek())) {
+					output.append(stack.pop()).append(' ');
 				}
+				stack.push(s);
 			}
-			// If the token is an operator, o1, then:
-				// while there is an operator token, o2, at the top of the operator stack, and either
-					// o1 is left-associative and its precedence is less than or equal to that of o2, or
-					// o1 is right associative, and has precedence less than that of o2,
-				// then pop o2 off the operator stack, onto the output queue;
-				// push o1 onto the operator stack.
-			// If the token is a left parenthesis (i.e. "("), then push it onto the stack.
-			// If the token is a right parenthesis (i.e. ")"):
-				// Until the token at the top of the stack is a left parenthesis, pop operators off the stack onto the output queue.
-				// Pop the left parenthesis from the stack, but not onto the output queue.
-				// If the token at the top of the stack is a function token, pop it onto the output queue.
-				// If the stack runs out without finding a left parenthesis, then there are mismatched parentheses.
-			// When there are no more tokens to read:
-				// While there are still operator tokens in the stack:
-					// If the operator token on the top of the stack is a parenthesis, then there are mismatched parentheses.
-					// Pop the operator onto the output queue.
-			// Exit.
+			// push function onto the stack
+			else if (functions.containsKey(s)) {
+				stack.push(s);
+			}
+			// push left parenthesis onto the stack
+			else if (s.equals("(")){
+				stack.push(s);
+			}
+			// on reaching right parenthesis, pop tokens off the stack onto the
+			// output queue until left parenthesis
+			else if (s.equals(")")) {
+				while (!stack.peek().equals("(")) {
+					output.append(stack.pop()).append(' ');
+					
+					if (stack.isEmpty()) {
+						System.err.println("Mismatched parentheses");
+					}
+				}
+				stack.pop(); // pop left parenthesis
+			}
+			// push integer on the output queue
+			else {
+				output.append(s);
+			}
+			
+			// pop the rest of the operators off the stack onto the output queue
+			while (!stack.isEmpty()) {
+				if (stack.peek().equals("(") || stack.peek().equals(")")) {
+					System.err.println("Mismatched parentheses");
+				}
+				
+				output.append(stack.pop()).append(' ');
+			}
 		}
 			
-		return rpnExpr;
+		return output.toString();
 	}
 
 }
